01d78785a8a5d4cb79e5c729a530d7e97f145d30
Emmanuel Ajuelos <60262354+ajuelosemmanuel@users.noreply.github.com>
2021-04-22 15:14:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-token = #YOUR TOKEN
+import os
+token = os.environ['DISCORD_TOKEN']
+# You can remove "os.environ['DISCORD_TOKEN']" and "import os" and simply place you token as a string after "token ="
