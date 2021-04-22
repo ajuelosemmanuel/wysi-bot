e055108f21faac97e8657432fbbd6a6a62ced371
Emmanuel Ajuelos <60262354+ajuelosemmanuel@users.noreply.github.com>
2021-04-22 15:59:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,20 +18,19 @@ async def on_ready():
 async def on_message(message):
     msg = message.content.lower()
     if "727" in msg:
-        await message.channel.send(content="WYSI")
         rng = random.randint(0, 1000)
         if rng == 727:
-            await message.channel.send(content=None, tts=False, embed=None, file=File('./media/bz.jpg'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
+            await message.channel.send(content="WYSI", tts=False, embed=None, file=File('./media/bz.jpg'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
         elif 695 <= rng <= 705:
-            await message.channel.send(content=None, tts=False, embed=None, file=File('./media/cookingz.png'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
+            await message.channel.send(content="WYSI", tts=False, embed=None, file=File('./media/cookingz.png'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
         elif 0 <= rng <= 100:
-            await message.channel.send(content=None, tts=False, embed=None, file=File('./media/wysicaps.jpg'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
+            await message.channel.send(content="WYSI", tts=False, embed=None, file=File('./media/wysicaps.jpg'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
         elif 101 <= rng <= 111:
-            await message.channel.send(content=None, tts=False, embed=None, file=File('./media/donaldwysi.png'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
+            await message.channel.send(content="WYSI", tts=False, embed=None, file=File('./media/donaldwysi.png'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
         elif 990 <= rng <= 1000:
-            await message.channel.send(content=None, tts=False, embed=None, file=File('./media/dicarpacciowysi.jpg'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
+            await message.channel.send(content="WYSI", tts=False, embed=None, file=File('./media/dicarpacciowysi.jpg'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
         else:
-            await message.channel.send(content=None, tts=False, embed=None, file=File('./media/wysiaireu.gif'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
+            await message.channel.send(content="WYSI", tts=False, embed=None, file=File('./media/wysiaireu.gif'), files=None, delete_after=None, nonce=None, allowed_mentions=None, reference=None, mention_author=None)
     await client.process_commands(message)
 
 
